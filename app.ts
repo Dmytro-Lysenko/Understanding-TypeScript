a202a44070bd194be205ea3302f8d60d051d8bf0
Dmytro Lysenko <crystaljoint@gmail.com>
2021-05-06 14:57:20 +0300

    adding typescript types

--- app.ts
+++ app.ts
@@ -1,1 +1,37 @@
-console.log("Your log here...");
+// const person: {
+//   name: string;
+//   age: number;
+// } = {
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string];
+// } = {
+//   name: "Dmytro",
+//   age: 30,
+//   hobbies: ["Sports", "Cooking"],
+//   role: [2, "author"],
+// };
+
+const person: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  role: [number, string];
+} = {
+  name: "Dmytro",
+  age: 30,
+  hobbies: ["Sports", "Cooking"],
+  role: [2, "author"],
+};
+// person.role.push("admin");
+// person.role[1] = 10;
+let favoriteAcrivites: string[];
+favoriteAcrivites = ["String"];
+console.log(person.name);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase());
+  //   console.log(hobby.map());
+}
